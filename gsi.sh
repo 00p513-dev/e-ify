e1c05df6182f1db62a7ceb471705e51d20b51517
Amy Grace <54113654+00p513-dev@users.noreply.github.com>
2020-06-16 14:46:32 +0100

    Update gsi.sh

--- gsi.sh
+++ gsi.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 echo "Welcome to e-ify by Amy (00p513)!"
 echo
-echo "Make sure you have flashed lineage os, your device is SAR, you are in TWRP, and have resized and mounted your system partition"
+echo "Make sure your e image is mounted /src and lineage /src"
 
 echo
 
@@ -12,41 +12,26 @@ read -p "Are you sure you want to continue? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-	adb push app/AccountManager/AccountManager.apk /system/system/app/AccountManager/AccountManager.apk
-	adb push app/Apps/Apps.apk /system/system/app/Apps/Apps.apk
-	adb push app/MagicEarth/MagicEarth.apk /system/system/app/MagicEarth/MagicEarth.apk
-	adb push app/Message/Message.apk /system/system/app/Message/Message.apk
-	adb push app/Notes/Notes.apk /system/system/app/Notes/Notes.apk
-	adb push app/Tasks/Tasks.apk /system/system/app/Tasks/Tasks.apk
-	adb push app/Weather/Weather.apk /system/system/app/Tasks/Tasks.apk
-	adb push app/Calendar/Calendar.apk /system/system/app/Calendar/Calendar.apk
-	adb push priv-app/BlissLauncher/BlissLauncher.apk /system/system/app/BlissLauncher/BlissLauncher.apk
-	adb push app/BlissIconPack/BlissIconPack.apk /system/app/BlissIconPack/BlissIconPack.apk
-	adb shell "rm /system/system/product/app/messaging/messaging.apk"
-	adb shell "rm /system/system/product/app/messaging/"
-	adb push media/bootanimation.zip /system/system/media/bootanimation.zip
-	adb push app/WallpaperPicker/WallpaperPicker.apk /system/system/app/WallpaperPicker/WallpaperPicker.apk
-	adb push app/WallpaperBackup/WallpaperBackup.apk /system/system/app/WallpaperBackup/WallpaperBackup.apk
+	cp /src/app/AccountManager/AccountManager.apk /mnt/system/app/AccountManager/AccountManager.apk
+	cp /src/app/Apps/Apps.apk /mnt/system/app/Apps/Apps.apk
+	cp /src/app/MagicEarth/MagicEarth.apk /mnt/system/app/MagicEarth/MagicEarth.apk
+	cp /src/app/Message/Message.apk /mnt/system/app/Message/Message.apk
+	cp /src/app/Notes/Notes.apk /mnt/system/app/Notes/Notes.apk
+	cp /src/app/Tasks/Tasks.apk /mnt/system/app/Tasks/Tasks.apk
+	cp /src/app/Weather/Weather.apk /mnt/system/app/Tasks/Tasks.apk
+	cp /src/app/Calendar/Calendar.apk /mnt/system/app/Calendar/Calendar.apk
+	cp /src/priv-app/BlissLauncher/BlissLauncher.apk /mnt/system/app/BlissLauncher/BlissLauncher.apk
+	cp /src/app/BlissIconPack/BlissIconPack.apk /system/app/BlissIconPack/BlissIconPack.apk
+	rm /mnt/system/product/app/messaging/messaging.apk"
+	rm /mnt/system/product/app/messaging/"
+	cp /src/media/bootanimation.zip /mnt/system/media/bootanimation.zip
+	cp /src/app/WallpaperPicker/WallpaperPicker.apk /mnt/system/app/WallpaperPicker/WallpaperPicker.apk
+	cp /src/app/WallpaperBackup/WallpaperBackup.apk /mnt/system/app/WallpaperBackup/WallpaperBackup.apk
+	cp /src/app/Browser/Browser.apk /mnt/system/app/Browser/Browser.apk
 	echo
 	echo
 	echo "Your Lineage phone is nearly your e-ify phone!"
 
-	echo
-	echo "Apply any further mods you want then reboot system. Then press y when on setup screen"
-	read -p "Are you ready to continue? " -n 1 -r
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]
-	then
-		echo "Please click allow on your phone and do not follow normal setup yet!"
-		adb install app/BlissIconPack/BlissIconPack.apk
-		adb install app/BlissIconPack/BlissIconPack.apk
-		adb reboot
-		echo
-		echo
-		echo "Your Lineage phone is now your e-ify phone!"
-		echo "Please wait while it restarts to finalise changes then setup normally!"
-
-	fi
 fi
 
 
