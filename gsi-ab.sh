44eced1b95573e7c420f118db7a9660b76f9429e
Amy Grace <54113654+00p513-dev@users.noreply.github.com>
2020-06-16 14:49:36 +0100

    Update gsi-ab.sh

--- gsi-ab.sh
+++ gsi-ab.sh
@@ -12,22 +12,22 @@ read -p "Are you sure you want to continue? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-	cp /src/app/AccountManager/AccountManager.apk /mnt/system/app/AccountManager/AccountManager.apk
-	cp /src/app/Apps/Apps.apk /mnt/system/app/Apps/Apps.apk
-	cp /src/app/MagicEarth/MagicEarth.apk /mnt/system/app/MagicEarth/MagicEarth.apk
-	cp /src/app/Message/Message.apk /mnt/system/app/Message/Message.apk
-	cp /src/app/Notes/Notes.apk /mnt/system/app/Notes/Notes.apk
-	cp /src/app/Tasks/Tasks.apk /mnt/system/app/Tasks/Tasks.apk
-	cp /src/app/Weather/Weather.apk /mnt/system/app/Tasks/Tasks.apk
-	cp /src/app/Calendar/Calendar.apk /mnt/system/app/Calendar/Calendar.apk
-	cp /src/priv-app/BlissLauncher/BlissLauncher.apk /mnt/system/app/BlissLauncher/BlissLauncher.apk
-	cp /src/app/BlissIconPack/BlissIconPack.apk /system/app/BlissIconPack/BlissIconPack.apk
-	rm /mnt/system/product/app/messaging/messaging.apk"
-	rm /mnt/system/product/app/messaging/"
-	cp /src/media/bootanimation.zip /mnt/system/media/bootanimation.zip
-	cp /src/app/WallpaperPicker/WallpaperPicker.apk /mnt/system/app/WallpaperPicker/WallpaperPicker.apk
-	cp /src/app/WallpaperBackup/WallpaperBackup.apk /mnt/system/app/WallpaperBackup/WallpaperBackup.apk
-	cp /src/app/Browser/Browser.apk /mnt/system/app/Browser/Browser.apk
+	cp /src/app/AccountManager/AccountManager.apk /mnt/system/system/app/AccountManager/AccountManager.apk
+	cp /src/app/Apps/Apps.apk /mnt/system/system/app/Apps/Apps.apk
+	cp /src/app/MagicEarth/MagicEarth.apk /mnt/system/system/app/MagicEarth/MagicEarth.apk
+	cp /src/app/Message/Message.apk /mnt/system/system/app/Message/Message.apk
+	cp /src/app/Notes/Notes.apk /mnt/system/system/app/Notes/Notes.apk
+	cp /src/app/Tasks/Tasks.apk /mnt/system/system/app/Tasks/Tasks.apk
+	cp /src/app/Weather/Weather.apk /mnt/system/system/app/Tasks/Tasks.apk
+	cp /src/app/Calendar/Calendar.apk /mnt/system/system/app/Calendar/Calendar.apk
+	cp /src/priv-app/BlissLauncher/BlissLauncher.apk /mnt/system/system/app/BlissLauncher/BlissLauncher.apk
+	cp /src/app/BlissIconPack/BlissIconPack.apk /system/system/app/BlissIconPack/BlissIconPack.apk
+	rm /mnt/system/system/product/app/messaging/messaging.apk
+	rm /mnt/system/system/product/app/messaging/
+	cp /src/media/bootanimation.zip /mnt/system/system/media/bootanimation.zip
+	cp /src/app/WallpaperPicker/WallpaperPicker.apk /mnt/system/system/app/WallpaperPicker/WallpaperPicker.apk
+	cp /src/app/WallpaperBackup/WallpaperBackup.apk /mnt/system/system/app/WallpaperBackup/WallpaperBackup.apk
+	cp /src/app/Browser/Browser.apk /mnt/system/system/app/Browser/Browser.apk
 	echo
 	echo
 	echo "Your Lineage phone is nearly your e-ify phone!"
