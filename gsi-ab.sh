17957879ed756b16744b087831ee58d129bd64b3
Amy Grace <54113654+00p513-dev@users.noreply.github.com>
2020-06-16 15:08:12 +0100

    Update gsi-ab.sh

--- gsi-ab.sh
+++ gsi-ab.sh
@@ -12,22 +12,35 @@ read -p "Are you sure you want to continue? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-	cp --parents /src/app/AccountManager/AccountManager.apk /mnt/system/system/app/AccountManager/
-	cp --parents /src/app/Apps/Apps.apk /mnt/system/system/app/Apps/
-	cp --parents /src/app/MagicEarth/MagicEarth.apk /mnt/system/system/app/MagicEarth/
-	cp --parents /src/app/Message/Message.apk /mnt/system/system/app/Message/
-	cp --parents /src/app/Notes/Notes.apk /mnt/system/system/app/Notes/
-	cp --parents /src/app/Tasks/Tasks.apk /mnt/system/system/app/Tasks/
-	cp --parents /src/app/Weather/Weather.apk /mnt/system/system/app/Tasks/
-	cp --parents /src/app/Calendar/Calendar.apk /mnt/system/system/app/Calendar/
-	cp --parents /src/priv-app/BlissLauncher/BlissLauncher.apk /mnt/system/system/app/BlissLauncher/
-	cp --parents /src/app/BlissIconPack/BlissIconPack.apk /system/system/app/BlissIconPack/
+	mkdir -p /mnt/system/system/app/AccountManager/
+	cp /src/app/AccountManager/AccountManager.apk /mnt/system/system/app/AccountManager/AccountManager.apk
+	mkdir -p /mnt/system/system/app/Apps/
+	cp /src/app/Apps/Apps.apk /mnt/system/system/app/Apps/Apps.apk
+	mkdir -p /mnt/system/system/app/MagicEarth/
+	cp /src/app/MagicEarth/MagicEarth.apk /mnt/system/system/app/MagicEarth/MagicEarth.apk
+	mkdir -p /mnt/system/system/app/Message
+	cp /src/app/Message/Message.apk /mnt/system/system/app/Message/Message.apk
+	mkdir -p /mnt/system/system/app/Notes
+	cp /src/app/Notes/Notes.apk /mnt/system/system/app/Notes/Notes.apk
+	mkdir -p /mnt/system/system/app/Tasks/
+	cp /src/app/Tasks/Tasks.apk /mnt/system/system/app/Tasks/Tasks.apk
+	mkdir -p /mnt/system/system/app/Weather
+	cp /src/app/Weather/Weather.apk /mnt/system/system/app/Tasks/Weather.apk
+	mkdir -p /mnt/system/system/app/Calendar
+	cp /src/app/Calendar/Calendar.apk /mnt/system/system/app/Calendar/Calendar.apk
+	mkdir -p /mnt/system/system/app/BlissLauncher/
+	cp /src/priv-app/BlissLauncher/BlissLauncher.apk /mnt/system/system/app/BlissLauncher/BlissLauncher.apk
+	mkdir -p /mnt/system/system/app/BlissIconPack/
+	cp /src/app/BlissIconPack/BlissIconPack.apk /system/system/app/BlissIconPack/BlissIconPack.apk
 	rm /mnt/system/system/product/app/messaging/messaging.apk
 	rm /mnt/system/system/product/app/messaging/
-	cp --parents /src/media/bootanimation.zip /mnt/system/system/media/
-	cp --parents /src/app/WallpaperPicker/WallpaperPicker.apk /mnt/system/system/app/WallpaperPicker/
-	cp --parents /src/app/WallpaperBackup/WallpaperBackup.apk /mnt/system/system/app/WallpaperBackup/
-	cp --parents /src/app/Browser/Browser.apk /mnt/system/system/app/Browser/
+	cp /src/media/bootanimation.zip /mnt/system/system/media/bootanimation.zip
+	mkdir -p /mnt/system/system/app/WallpaperPicker
+	cp /src/app/WallpaperPicker/WallpaperPicker.apk /mnt/system/system/app/WallpaperPicker/WallpaperPicker.apk
+	mkdir -p /mnt/system/system/app/WallpaperBackup
+	cp /src/app/WallpaperBackup/WallpaperBackup.apk /mnt/system/system/app/WallpaperBackup/WallpaperBackup.apk
+	mkdir -p /mnt/system/system/app/Browser/
+	cp /src/app/Browser/Browser.apk /mnt/system/system/app/Browser/Browser.apk
 	echo
 	echo
 	echo "Your Lineage phone is nearly your e-ify phone!"
